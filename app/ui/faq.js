d0d884a16979db59a80f50c06beaf72ca7a1a0a2
tanshiyu <tanshiyu1999@gmail.com>
2024-05-25 15:09:06 +0800

    add target

--- app/ui/faq.js
+++ app/ui/faq.js
@@ -13,7 +13,7 @@ export default function Faq() {
       <h1 className="text-5xl my-3">FAQ & T&C:</h1>
       <p className="max-w-[1100px] text-xl mb-7 mt-5 text-center px-5">In light of the qualitative and quantitative indicators as stated above, NUSPS has constructed a robust Frequently Asked Questions (FAQs), Terms and Conditions (T&C) and Regulations section as follows:</p>
       <div className="flex flex-row flex-wrap min-w-96 max-w-[85%] items-center justify-center">
-        <a href="https://montage2024.vercel.app/faq.pdf">
+        <a href="https://montage2024.vercel.app/faq.pdf" target="_blank">
           <div className="flex flex-col items-center p-3 rounded-xl
             h-[200px] w-96 m-2 bg-yellow-50 shadow shadow-black 
             cursor-pointer"
@@ -23,7 +23,7 @@ export default function Faq() {
           </div>
         </a>
 
-        <a href="https://montage2024.vercel.app/tac.pdf">
+        <a href="https://montage2024.vercel.app/tac.pdf" target="_blank">
           <div className="flex flex-col items-center p-3 rounded-xl
             h-[200px] w-96 m-2 bg-yellow-50 shadow shadow-black 
             cursor-pointer"
@@ -33,7 +33,7 @@ export default function Faq() {
           </div>
         </a>
 
-        <a href="https://montage2024.vercel.app/rar.pdf">
+        <a href="https://montage2024.vercel.app/rar.pdf" target="_blank">
           <div className="flex flex-col items-center p-3 rounded-xl
             h-[200px] w-96 m-2 bg-yellow-50 shadow shadow-black 
             cursor-pointer"
