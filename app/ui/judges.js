a4ad7188673f3d8023723d40c107ba4cb92c423c
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2024-05-29 22:20:12 +0800

    Fix judges & timeline

--- app/ui/judges.js
+++ app/ui/judges.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import Popup from "reactjs-popup";
@@ -8,10 +8,10 @@ export default function Judges() {
     <div className="flex flex-col items-center" id="Judges">
       <h1 className="text-7xl text-center m-10">Judges</h1>
       <div className="flex flex-row flex-wrap w-full lg:max-w-[85%] items-center justify-center">
-        <div className="flex flex-col items-center p-3 rounded-xl
-          h-[400px] max-w-96 m-2 bg-yellow-50 shadow shadow-black 
-        ">
-          <Image 
+        <div
+          className="flex flex-col items-center p-3 rounded-xl
+          h-[400px] max-w-96 m-2">
+          <Image
             src="https://i.imgur.com/9xWRkmY.jpg"
             width={175}
             height={300}
@@ -19,19 +19,19 @@ export default function Judges() {
           />
           <h2 className="text-3xl">Jasper Yu</h2>
           <p className="text-md h-24 px-5">
-            Founder of JYU STUDIO, Jasper is a professional photographer of 11 years with a fine arts background.
+            Founder of JYU STUDIO, Jasper is a professional photographer of 11
+            years with a fine arts background.
           </p>
 
           <hr className="w-64 border-t-2 border-black m-1" />
           <a href="https://www.jyu.sg/" target="_blank">
             <p className="text-xl cursor-pointer">Learn More!</p>
           </a>
-
         </div>
-        <div className="flex flex-col items-center p-3 rounded-xl
-          h-[400px] max-w-96 m-2 bg-yellow-50 shadow shadow-black 
-        ">
-          <Image 
+        <div
+          className="flex flex-col items-center p-3 rounded-xl
+          h-[400px] max-w-96 m-2">
+          <Image
             src="https://i.imgur.com/YA0mIb6.png"
             width={160}
             height={175}
@@ -39,19 +39,19 @@ export default function Judges() {
           />
           <h2 className="text-3xl">Carlos Monforte</h2>
           <p className="text-md h-24 px-5">
-            Carlos is passionate photographer, capturing light and emotions in unique portraits and panoramic landscapes.
+            Carlos is passionate photographer, capturing light and emotions in
+            unique portraits and panoramic landscapes.
           </p>
 
           <hr className="w-64 border-t-2 border-black m-1" />
           <a href="https://www.instagram.com/lightjunker/" target="_blank">
             <p className="text-xl cursor-pointer">Learn More!</p>
           </a>
-
         </div>
-        <div className="flex flex-col items-center p-3 rounded-xl
-          h-[400px] max-w-96 m-2 bg-yellow-50 shadow shadow-black 
-        ">
-          <Image 
+        <div
+          className="flex flex-col items-center p-3 rounded-xl
+          h-[400px] max-w-96 m-2">
+          <Image
             src="https://i.imgur.com/m1556Sf.jpg"
             width={190}
             height={300}
@@ -59,15 +59,16 @@ export default function Judges() {
           />
           <h2 className="text-3xl">Francis Lee</h2>
           <p className="text-md h-24 px-5">
-            Francis is a photographer with 20 plus years of experience, and currently an adjunt lecturer on Creative Photography at Orita Sinclair School.
+            Francis is a photographer with 20 plus years of experience, and
+            currently an adjunt lecturer on Creative Photography at Orita
+            Sinclair School.
           </p>
 
           <hr className="w-64 border-t-2 border-black m-1" />
           <a href="https://www.instagram.com/francislee62/" target="_blank">
             <p className="text-xl cursor-pointer">Learn More!</p>
-          </a>        
+          </a>
         </div>
-
       </div>
     </div>
   );
--- app/ui/timeline.js
+++ app/ui/timeline.js
@@ -4,121 +4,122 @@ import Image from "next/image";
 
 export default function Timeline() {
   return (
-    <section id="Timeline" className="flex justify-center">
-      <div className="flex flex-col items-left w-11/12 lg:w-9/12 max-w-[1200px]">
-        <h1 className="self-center text-5xl mt-4">Workshops</h1>
-
-        <div
-          className="card-container grid grid-cols-1 xl:grid-cols-5	place-items-center
+    <div>
+      <section id="Timeline" className="flex justify-center">
+        <div className="flex flex-col items-left w-11/12 lg:w-9/12 max-w-[1200px]">
+          <h1 className="self-center text-5xl mt-4">Workshops</h1>
+          <div
+            className="card-container grid grid-cols-1 xl:grid-cols-5	place-items-center
            pt-5 my-1">
-          <div className="col-span-1 flex justify-center items-center mx-5">
-            <h1 className="text-4xl">1 June</h1>
-          </div>
-          <div className="col-span-4 m-5 flex flex-col items-center w-9/12 self-center">
-            <h1 className="text-3xl mb-3 font-bold">
-              Portraiture & Lighting Workshop
-            </h1>
-            <Image
-              src="https://i.imgur.com/GuthkTF.png"
-              width={600}
-              height={600}
-              alt="Portrait workshop Image"
-            />
-            <div className="mt-2">
-              <p>
-                Dive into the world of professional portraiture at our workshop!
-                Learn how to master lighting setups and backdrop selections to
-                dramatically enhance your subject's features and emotions.
-              </p>
-              <br />
-              <p className="font-bold">Time</p>
-              <p>09:00 - 12:00 (Session 1)</p>
-              <p>15:00 - 18:00 (Session 2)</p>
-              <br />
-              <p className="font-bold">Venue</p>
-              <p>Haste Studio (6 Ubi Road 1, #02-03, S408726)</p>
+            <div className="col-span-1 flex justify-center items-center mx-5">
+              <h1 className="text-4xl">1 June</h1>
             </div>
-            <a
-              href="https://nus.campuslabs.com/engage/submitter/form/start/640774"
-              target="_blank">
-              <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-5">
-                Sign Up
-              </button>
-            </a>
+            <div className="col-span-4 m-5 flex flex-col items-center w-9/12 self-center">
+              <h1 className="text-3xl mb-3 font-bold">
+                Portraiture & Lighting Workshop
+              </h1>
+              <Image
+                src="https://i.imgur.com/GuthkTF.png"
+                width={600}
+                height={600}
+                alt="Portrait workshop Image"
+              />
+              <div className="mt-2">
+                <p>
+                  Dive into the world of professional portraiture at our
+                  workshop! Learn how to master lighting setups and backdrop
+                  selections to dramatically enhance your subject's features and
+                  emotions.
+                </p>
+                <br />
+                <p className="font-bold">Time</p>
+                <p>09:00 - 12:00 (Session 1)</p>
+                <p>15:00 - 18:00 (Session 2)</p>
+                <br />
+                <p className="font-bold">Venue</p>
+                <p>Haste Studio (6 Ubi Road 1, #02-03, S408726)</p>
+              </div>
+              <a
+                href="https://nus.campuslabs.com/engage/submitter/form/start/640774"
+                target="_blank">
+                <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-5">
+                  Sign Up
+                </button>
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div
-          className="card-container grid grid-cols-1 xl:grid-cols-5	place-items-center
+          <div
+            className="card-container grid grid-cols-1 xl:grid-cols-5	place-items-center
            pt-5 my-3">
-          <div className="col-span-1 flex justify-center items-center mx-10">
-            <h1 className="text-4xl">15 June</h1>
-          </div>
-          <div className="col-span-4 m-5 flex flex-col items-center w-9/12 self-center">
-            <h1 className="text-3xl mb-3 font-bold">
-              Street Photography Workshop
-            </h1>
-            {/* <Image
+            <div className="col-span-1 flex justify-center items-center mx-10">
+              <h1 className="text-4xl">15 June</h1>
+            </div>
+            <div className="col-span-4 m-5 flex flex-col items-center w-9/12 self-center">
+              <h1 className="text-3xl mb-3 font-bold">
+                Street Photography Workshop
+              </h1>
+              {/* <Image
               src="TBA"
               width={600}
               height={600}
               alt="Portrait workshop Image"
             /> */}
-            <div className="mt-2">
-              <p>
-                Hit the streets with confidence! This workshop will enhance your
-                skills in capturing candid moments and engaging quickly with
-                subjects, ensuring you never miss a shot.
-              </p>
-              <br />
-              <p>Stay tuned for more details.</p>
-            </div>
-            {/* <a
+              <div className="mt-2">
+                <p>
+                  Hit the streets with confidence! This workshop will enhance
+                  your skills in capturing candid moments and engaging quickly
+                  with subjects, ensuring you never miss a shot.
+                </p>
+                <br />
+                <p>Stay tuned for more details.</p>
+              </div>
+              {/* <a
               href="TBA"
               target="_blank">
               <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-5">
                 Sign Up
               </button>
             </a> */}
+            </div>
           </div>
-        </div>
 
-        <div
-          className="card-container grid grid-cols-1 xl:grid-cols-5	place-items-center
+          <div
+            className="card-container grid grid-cols-1 xl:grid-cols-5	place-items-center
            pt-5 my-3">
-          <div className="col-span-1 flex justify-center items-center mx-10">
-            <h1 className="text-4xl">29 June</h1>
-          </div>
-          <div className="col-span-4 m-5 flex flex-col items-center w-9/12 self-center">
-            <h1 className="text-3xl mb-3 font-bold">
-              Street Photography Workshop
-            </h1>
-            {/* <Image
+            <div className="col-span-1 flex justify-center items-center mx-10">
+              <h1 className="text-4xl">29 June</h1>
+            </div>
+            <div className="col-span-4 m-5 flex flex-col items-center w-9/12 self-center">
+              <h1 className="text-3xl mb-3 font-bold">
+                Street Photography Workshop
+              </h1>
+              {/* <Image
               src="TBA"
               width={600}
               height={600}
               alt="Portrait workshop Image"
             /> */}
-            <div className="mt-2">
-              <p>
-                Capture the world as you see it! Learn to compose and create
-                breathtaking travel narratives, transforming natural and
-                bustling environments into stunning photographic stories.
-              </p>
-              <br />
-              <p>Stay tuned for more details.</p>
-            </div>
-            {/* <a
+              <div className="mt-2">
+                <p>
+                  Capture the world as you see it! Learn to compose and create
+                  breathtaking travel narratives, transforming natural and
+                  bustling environments into stunning photographic stories.
+                </p>
+                <br />
+                <p>Stay tuned for more details.</p>
+              </div>
+              {/* <a
               href="TBA"
               target="_blank">
               <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-5">
                 Sign Up
               </button>
             </a> */}
+            </div>
           </div>
         </div>
-
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
