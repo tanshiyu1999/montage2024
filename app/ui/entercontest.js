0069ea0559c8586dae1a8681ada669677d189c2f
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2024-05-30 00:13:27 +0800

    add links

--- app/ui/entercontest.js
+++ app/ui/entercontest.js
@@ -11,19 +11,27 @@ export default function EnterContest() {
         1. Download and create an account on the Klick Photography App.
       </p>
       <div className="p-3 flex flex-col lg:flex-row">
-        <Image
-          src="/klick-appstore-qr.jpg"
-          alt="QR Code 1"
-          width={300}
-          height={300}
-          className="lg:mr-5 mb-5 lg:mb-0"
-        />
-        <Image
-          src="/klick-playstore-qr.jpg"
-          alt="QR Code 2"
-          width={300}
-          height={300}
-        />
+        <a
+          href="https://apps.apple.com/sg/app/klick-photo-sharing/id1673602702"
+          target="_blank">
+          <Image
+            src="/klick-appstore-qr.jpg"
+            alt="QR Code 1"
+            width={300}
+            height={300}
+            className="lg:mr-5 mb-5 lg:mb-0"
+          />{" "}
+        </a>
+        <a
+          href="https://play.google.com/store/apps/details?id=sg.klick.android"
+          target="_blank">
+          <Image
+            src="/klick-playstore-qr.jpg"
+            alt="QR Code 2"
+            width={300}
+            height={300}
+          />
+        </a>
       </div>
       <p className="text-xl p-3">
         2. Post your photo on Klick with the tag #NUSPSMontage2024.
@@ -44,14 +52,14 @@ export default function EnterContest() {
       <div className="flex flex-row justify-center space-x-20 p-3 pb-5">
         <a
           className="text-xl no-underline hover:underline text-blue-600"
-          href=""
+          href="https://tinyurl.com/montage2024-rr"
           target="_blank">
           {" "}
           Rules and Regulations
         </a>
         <a
           className="text-xl no-underline hover:underline text-blue-600"
-          href=""
+          href="https://tinyurl.com/montage2024-tcs"
           target="_blank">
           {" "}
           Terms and Conditions
