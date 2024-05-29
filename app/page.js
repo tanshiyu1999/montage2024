a430590bed1be9d2b4ae3050427403ffff5e451d
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2024-05-29 22:58:41 +0800

    Fix FAQ

--- app/page.js
+++ app/page.js
@@ -34,8 +34,8 @@ export default function Home() {
       <EnterContest />
       <Timeline />
       <Judges />
-      <History /> 
       <Faq />
+      <History /> 
       <Footer />
     </main>
   );}
