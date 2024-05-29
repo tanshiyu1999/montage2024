eb7e0d3009ca0a0d7ee2955b2049a381ed9af2bd
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2024-05-29 22:15:04 +0800

    Add contest entering details

--- app/page.js
+++ app/page.js
@@ -1,15 +1,14 @@
-import Image from "next/image";
+
 import Landing from "./ui/landing";
 import Theme from "./ui/theme";
 import History from "./ui/history";
 import Timeline from "./ui/timeline";
 import Faq from "./ui/faq";
 import Judges from "./ui/judges";
-import { metadata } from "./layout";
 import Navbar from "./ui/navbar";
 import Footer from "./ui/footer";
-import Tac from "./ui/tac";
 import "./main.css"
+import EnterContest from "./ui/entercontest";
 
 
 // Images https://imgur.com/a/ctjRBeR
@@ -32,8 +31,9 @@ export default function Home() {
       <Navbar />
       <Landing />
       <Theme />
-      <Judges />
+      <EnterContest />
       <Timeline />
+      <Judges />
       <History /> 
       <Faq />
       <Footer />
